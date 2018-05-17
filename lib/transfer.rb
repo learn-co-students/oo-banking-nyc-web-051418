5048439c73d57f92314cfe87a3bf2b153a46ccdf
github username <github email address>
2018-05-17 16:34:42 -0400

    working on last method

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,28 @@
+require 'pry'
+require 'bank_account'
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  def valid?
+    @sender.valid? == @receiver.valid?
+  end
+  def execute_transaction
+    if @sender.valid? == true
+        if @status == "pending"
+          @sender.balance -= @amount
+          @receiver.balance += @amount
+          @status = "complete"
+        else
+          false
+        end
+      else "Transaction rejected. Please check your account balance."
+      end
+  end
 end
